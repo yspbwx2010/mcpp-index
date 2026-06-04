9ae20f37d1c104ee44032ad73d6c96a6f8a99e0b
SPeak <speakshen@163.com>
2026-06-04 08:30:22 +0800

    compat.imgui: add 1.92.8-docking (upstream docking branch tag) (#36)
    Same mcpp build segment (glob sources); enables imgui-m's 'docking' feature.
    1.92.8 (master) untouched.
    
    Co-authored-by: sunrisepeak <x.d2learn.org@gmail.com>

--- pkgs/c/compat.imgui.lua
+++ pkgs/c/compat.imgui.lua
@@ -13,17 +13,29 @@ package = {
                 url    = "https://github.com/ocornut/imgui/archive/refs/tags/v1.92.8.tar.gz",
                 sha256 = "fecb33d33930e12ff53a34064e9d3a06c8f7c3e04408f14cd36c80e3faac863b",
             },
+            ["1.92.8-docking"] = {
+                url    = "https://github.com/ocornut/imgui/archive/refs/tags/v1.92.8-docking.tar.gz",
+                sha256 = "ca0653454ed371b7a87e9b0bc29a5d15c9be7f7c0fbe778042fc48c71df1d3d8",
+            },
         },
         macosx = {
             ["1.92.8"] = {
                 url    = "https://github.com/ocornut/imgui/archive/refs/tags/v1.92.8.tar.gz",
                 sha256 = "fecb33d33930e12ff53a34064e9d3a06c8f7c3e04408f14cd36c80e3faac863b",
             },
+            ["1.92.8-docking"] = {
+                url    = "https://github.com/ocornut/imgui/archive/refs/tags/v1.92.8-docking.tar.gz",
+                sha256 = "ca0653454ed371b7a87e9b0bc29a5d15c9be7f7c0fbe778042fc48c71df1d3d8",
+            },
         },
         windows = {
             ["1.92.8"] = {
                 url    = "https://github.com/ocornut/imgui/archive/refs/tags/v1.92.8.tar.gz",
                 sha256 = "fecb33d33930e12ff53a34064e9d3a06c8f7c3e04408f14cd36c80e3faac863b",
+            },
+            ["1.92.8-docking"] = {
+                url    = "https://github.com/ocornut/imgui/archive/refs/tags/v1.92.8-docking.tar.gz",
+                sha256 = "ca0653454ed371b7a87e9b0bc29a5d15c9be7f7c0fbe778042fc48c71df1d3d8",
             },
         },
     },
